5116478bea8ae215433d3c7b1bcf631f911e2061
muhammad-abdulloh <clevercoderr@gmail.com>
2023-10-23 14:54:41 +0500

    Staging and Production environment ajratildi

--- TaskSoliq/Program.cs
+++ TaskSoliq/Program.cs
@@ -13,10 +13,19 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUserServices, UserServices>();
 
-builder.Services.AddDbContext<TurniketDbContext>(option =>
+if (builder.Environment.IsDevelopment())
 {
-    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+    /* Update-Database */
+    builder.Services.AddDbContext<TurniketDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("StagingConnection") ?? throw new InvalidOperationException("Connection string 'StagingConnection' not found.")));
+}
+else
+{
+    /* Update-Database -Args '--environment Production' */
+
+    builder.Services.AddDbContext<TurniketDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionConnection") ?? throw new InvalidOperationException("Connection string 'ProductionConnection' not found.")));
+}
 
 
 var app = builder.Build();
@@ -31,6 +40,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.MapControllers();
 
