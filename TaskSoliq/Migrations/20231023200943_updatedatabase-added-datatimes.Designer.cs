48926527bf16fc6409c7d83aaaf6cdc84ba77979
muhammad-abdulloh <clevercoderr@gmail.com>
2023-10-24 01:11:36 +0500

    add migration

--- TaskSoliq/Migrations/20231023200943_updatedatabase-added-datatimes.Designer.cs
+++ TaskSoliq/Migrations/20231023200943_updatedatabase-added-datatimes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskSoliq.Infrastructure;
 
@@ -11,9 +12,11 @@ using TaskSoliq.Infrastructure;
 namespace TaskSoliq.Migrations
 {
     [DbContext(typeof(TurniketDbContext))]
-    partial class TurniketDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231023200943_updatedatabase-added-datatimes")]
+    partial class updatedatabaseaddeddatatimes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TaskSoliq/Migrations/20231023200943_updatedatabase-added-datatimes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskSoliq.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedatabaseaddeddatatimes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "Users",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedDate",
+                table: "Users",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ModifyDate",
+                table: "Users",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ModifyDate",
+                table: "Users");
+        }
+    }
+}
